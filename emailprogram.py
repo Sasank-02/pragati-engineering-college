acf62434f9d3dc83a3b51bf545616df1806dd0b8
Sasank-02 <124041123+Sasank-02@users.noreply.github.com>
2023-07-21 12:24:14 +0530

    Update emailprogram.py

--- emailprogram.py
+++ emailprogram.py
@@ -4,7 +4,7 @@ otp=5678
 cemail=str(input("Enter the email:"))
 cpwd=int(input("Enter the pwd:"))
 cotp=input("enter the otp:")
-if(email==cemail and pwd==cpwd):
+if(email==cemail and pwd==cpwd and otp==cotp):
     print("Login successfully")
 elif(email!=cemail and pwd==cpwd):
     print("login failed due to mail")
@@ -12,3 +12,5 @@ elif(email==cemail and pwd!=cpwd):
     print("login failed due to pwd")
 elif(email!=cemail and pwd!=cpwd):
     print("login failed due to both mail and pwd")
+elif(otp != cotp):
+    print("Login failed due to wrong otp")
